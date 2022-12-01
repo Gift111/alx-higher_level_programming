bf84695ac2f245d2773be5eb009e74c5daf86d87
Gift111 <giftchukwudi99@gmail.com>
2022-11-30 19:21:48 -0800

    update 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 """Print all possible different combinations of two digits in ascending order. The two digits must be different"""
-for 1stdigit in range(0, 10):
-    for 2nddigit in range(1stdigit + 1, 10):
-        if 1stdigit == 8 and 2nddigit == 9:
-            print("{}{}".format(1stdigit, 2nddigit))
+for 1digit in range(0, 10):
+    for 2digit in range(1digit + 1, 10):
+        if 1digit == 8 and 2digit == 9:
+            print("{}{}".format(1digit, 2digit))
         else:
-            print("{}{}".format(1stdigit, 2nddigit), end=",")
+            print("{}{}".format(1digit, 2digit), end=",")
